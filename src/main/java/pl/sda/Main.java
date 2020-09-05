5fdd3547830d8bcc9ab82e9cb0745992bec2264d
PrzemekWolosz <przemyslaw.wolosz1@gmail.com>
2020-09-05 10:59:01 +0200

    test

--- src/main/java/pl/sda/Main.java
+++ src/main/java/pl/sda/Main.java
@@ -9,7 +9,7 @@ public class Main {
     public static void main(String[] args) {
         ConsoleManager consoleManager = new ConsoleManager();
         consoleManager.start();
+
+        System.out.println("aaa");
     }
-
-
 }
